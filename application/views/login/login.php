40bd3798f1e537c0f5d4fd816005cb50588aa03f
pdc-carlomarxsumbay <carlo.marx.s.umbay@accenture.com>
2016-08-06 20:18:16 +0800

    Removed Unused files

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -12,16 +12,16 @@
     <title>UCare - Login</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo CuConfig::$siteUrl?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo CuConfig::$siteUrl?>assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="<?php echo CuConfig::$siteUrl?>assets/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="<?php echo CuConfig::$siteUrl?>assets/css/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="<?php echo CuConfig::$siteUrl?>assets/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo CuConfig::$siteUrl?>assets/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="<?php echo CuConfig::$siteUrl?>assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo CuConfig::$siteUrl?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -71,16 +71,16 @@
     </div>
 
     <!-- jQuery -->
-    <script src="<?php echo CuConfig::$siteUrl?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo CuConfig::$siteUrl?>assets/js/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo CuConfig::$siteUrl?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo CuConfig::$siteUrl?>assets/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?php echo CuConfig::$siteUrl?>assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="<?php echo CuConfig::$siteUrl?>assets/js/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="<?php echo CuConfig::$siteUrl?>assets/dist/js/sb-admin-2.js"></script>
+    <script src="<?php echo CuConfig::$siteUrl?>assets/js/sb-admin-2.js"></script>
 
 </body>
 
