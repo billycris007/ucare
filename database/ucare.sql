15effe88945f48c27ddbb0803a87521de914ce02
billycris <billycris007@gmail.com>
2016-08-07 02:00:56 +0800

    update database

--- database/ucare.sql
+++ database/ucare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2016 at 02:26 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
+-- Generation Time: Aug 06, 2016 at 08:00 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `ucare`
@@ -26,10 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `image`
 --
 
-CREATE TABLE `image` (
-  `image_id` int(11) NOT NULL,
-  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `image` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -37,13 +38,22 @@ CREATE TABLE `image` (
 -- Table structure for table `organization`
 --
 
-CREATE TABLE `organization` (
-  `org_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `organization` (
+  `org_id` int(5) NOT NULL AUTO_INCREMENT,
   `user_id` int(5) NOT NULL,
-  `org_name` int(255) NOT NULL,
+  `org_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `org_image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `org_description` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`org_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `organization`
+--
+
+INSERT INTO `organization` (`org_id`, `user_id`, `org_name`, `org_image`, `org_description`, `created_date`) VALUES
+(1, 2, 'Red Cross', '', 'Red Cross', '2016-08-06 17:27:53');
 
 -- --------------------------------------------------------
 
@@ -51,8 +61,8 @@ CREATE TABLE `organization` (
 -- Table structure for table `purpose`
 --
 
-CREATE TABLE `purpose` (
-  `purpose_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `purpose` (
+  `purpose_id` int(5) NOT NULL AUTO_INCREMENT,
   `org_id` int(5) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `long_lat` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -60,8 +70,18 @@ CREATE TABLE `purpose` (
   `delivery_date` datetime NOT NULL,
   `funds` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `url_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`purpose_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
+
+--
+-- Dumping data for table `purpose`
+--
+
+INSERT INTO `purpose` (`purpose_id`, `org_id`, `name`, `long_lat`, `type`, `delivery_date`, `funds`, `description`, `url_name`, `created_date`) VALUES
+(8, 1, 'Earthquake', '10.315699:123.885437', 'Earthquake', '2016-08-30 00:00:00', '', 'Help Build typhoon victims', '', '2016-08-06 17:49:50'),
+(11, 1, 'walay tugpahay * 101', '10.315699:123.885437', 'Fire', '2016-08-30 00:00:00', '', 'Help Build typhoon victims', 'walay-tugpahay--101', '2016-08-06 17:49:20');
 
 -- --------------------------------------------------------
 
@@ -69,13 +89,14 @@ CREATE TABLE `purpose` (
 -- Table structure for table `transaction`
 --
 
-CREATE TABLE `transaction` (
-  `transaction_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `transaction_id` int(5) NOT NULL AUTO_INCREMENT,
   `user_id` int(5) NOT NULL,
   `purpose_id` int(5) NOT NULL,
   `amount` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -83,13 +104,14 @@ CREATE TABLE `transaction` (
 -- Table structure for table `updates`
 --
 
-CREATE TABLE `updates` (
-  `update_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `updates` (
+  `update_id` int(5) NOT NULL AUTO_INCREMENT,
   `purpose_id` int(5) NOT NULL,
   `image_id` int(5) NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `posted_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `posted_date` datetime NOT NULL,
+  PRIMARY KEY (`update_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -97,97 +119,25 @@ CREATE TABLE `updates` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `user_id` int(5) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(5) NOT NULL AUTO_INCREMENT,
   `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `user_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `user_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `email`, `password`, `lastname`, `firstname`, `user_type`) VALUES
-(1, 'umbay.cm@gmail.com', 'abcd1234', 'Umbay', 'Carlo Marx', 'superadmin');
+(1, 'umbay.cm@gmail.com', 'abcd1234', 'Umbay', 'Carlo Marx', 'superadmin'),
+(2, 'billy@gmail.com', 'asdfg', 'Pilapil', 'Billy Cris', 'superadmin');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `image`
---
-ALTER TABLE `image`
-  ADD PRIMARY KEY (`image_id`);
-
---
--- Indexes for table `organization`
---
-ALTER TABLE `organization`
-  ADD PRIMARY KEY (`org_id`);
-
---
--- Indexes for table `purpose`
---
-ALTER TABLE `purpose`
-  ADD PRIMARY KEY (`purpose_id`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`transaction_id`);
-
---
--- Indexes for table `updates`
---
-ALTER TABLE `updates`
-  ADD PRIMARY KEY (`update_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `image`
---
-ALTER TABLE `image`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `organization`
---
-ALTER TABLE `organization`
-  MODIFY `org_id` int(5) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `purpose`
---
-ALTER TABLE `purpose`
-  MODIFY `purpose_id` int(5) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `transaction_id` int(5) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `updates`
---
-ALTER TABLE `updates`
-  MODIFY `update_id` int(5) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
