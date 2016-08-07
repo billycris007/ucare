7dd60a30251b2eea9d010a32d41e83cfe6d57e84
billycris <billycris007@gmail.com>
2016-08-07 09:20:54 +0800

    asdfasdf

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://ucare.dev/" class="navbar-brand">
+            <a class="navbar-brand" href="<?= CuConfig::$siteUrl; ?>/user" class="navbar-brand">
                 <img width="150px" height="150px" class="img-responsive" src="http://ucare.dev/ucare.png">
             </a>
         </div>
